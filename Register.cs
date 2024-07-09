c397226e08c12920aa8418934e52eccacad0c25e
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-09 15:36:05 -0700

    03_03e

--- Register.cs
+++ Register.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 
@@ -9,7 +9,8 @@ namespace WebsiteWatcher;
 public class Register(ILogger<Register> logger)
 {
     [Function(nameof(Register))]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    [SqlOutput("dbo.Websites", connectionStringSetting: "LocalSql")]
+    public async Task<Website> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -19,7 +20,7 @@ public class Register(ILogger<Register> logger)
         
         newWebsite.Id = Guid.NewGuid();
 
-        return new OkObjectResult(newWebsite);
+        return newWebsite;
     }
 }
 
@@ -27,5 +28,5 @@ public class Website
 {
     public Guid Id { get; set; }
     public string Url { get; set; }
-    public string? XpathExpression { get; set; }
+    public string? XPathExpression { get; set; }
 }
