9e2277d15db7032fbe50902b4ac4dd3b7ebef0f6
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-09 13:14:16 -0700

    03_02e

---
+++ Register.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace WebsiteWatcher;
+
+public class Register(ILogger<Register> logger)
+{
+    [Function(nameof(Register))]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+        Website newWebsite = JsonSerializer.Deserialize<Website>(requestBody, options);
+        newWebsite.Id = Guid.NewGuid();
+
+        return new OkObjectResult(newWebsite);
+    }
+}
+
+public class Website
+{
+    public Guid Id { get; set; }
+    public string Url { get; set; }
+    public string? XpathExpression { get; set; }
+}
