1c2282f7dca31cd39e8a93994e7cbfa76f2ba5a8
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-10 09:08:11 -0700

    03_05e

--- Snapshot.cs
+++ Snapshot.cs
@@ -1,3 +1,4 @@
+using HtmlAgilityPack;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
@@ -14,6 +15,20 @@ public class Snapshot(ILogger<Snapshot> logger)
         {
             logger.LogInformation($"{change.Operation}");
             logger.LogInformation($"Id: {change.Item.Id} Url: {change.Item.Url}");
+
+            if (change.Operation != SqlChangeOperation.Insert)
+            {
+                continue;
+            }
+
+            HtmlWeb web = new ();
+            HtmlDocument doc = web.Load(change.Item.Url);
+
+            var divWithContent = doc.DocumentNode.SelectSingleNode(change.Item.XPathExpression);
+            var content = divWithContent != null ? divWithContent.InnerText.Trim() : "No content";
+
+            logger.LogInformation(content);
+
         }
     }
 }
