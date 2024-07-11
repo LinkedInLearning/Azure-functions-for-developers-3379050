6ce66421b0ef769cfefcc52b6f33fad872a0c577
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-10 18:09:09 -0700

    03_07e

---
+++ PdfCreator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using Microsoft.Extensions.Logging;
+
+namespace WebsiteWatcher;
+
+public class PdfCreator(ILogger<PdfCreator> logger)
+{
+    [Function(nameof(PdfCreator))]
+    public async Task Run(
+      [SqlTrigger("dbo.Websites", "WebsiteWatcher")] SqlChange<Website>[] changes)
+    {
+        foreach (var change in changes)
+        {
+            if (change.Operation == SqlChangeOperation.Insert)
+            {
+                
+            }
+        }
+    }
+}
