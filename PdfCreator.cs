7f38ca1599c7ec0ba28732e081f2836e56220620
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-11 08:23:35 -0700

    03_08e

--- PdfCreator.cs
+++ PdfCreator.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿using Azure.Storage.Blobs;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
 using PuppeteerSharp;
@@ -16,7 +17,12 @@ public class PdfCreator(ILogger<PdfCreator> logger)
             if (change.Operation == SqlChangeOperation.Insert)
             {
                 var result = await ConvertPageToPdfAsync(change.Item.Url);
+
                 logger.LogInformation($"PDF stream length is: {result.Length}");
+
+                var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings:WebsiteWatcherStorage");
+                var blobClient = new BlobClient(connectionString, "pdfs", $"{change.Item.Id}.pdf");
+                await blobClient.UploadAsync(result);
             }
         }
     }
