760b558f2ba90f80d29debe27c0a234ae83f7e61
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-07-12 16:11:17 -0700

    04_03e

---
+++ SafeBrowsingMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteWatcher;
+
+public class SafeBrowsingMiddleware : IFunctionsWorkerMiddleware
+{
+    public Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        throw new NotImplementedException();
+    }
+}
